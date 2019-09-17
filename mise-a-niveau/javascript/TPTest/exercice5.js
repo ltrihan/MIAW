808e2e9665186de2263a11cca43635c7cbd4b28c
Loic Trihan <lolotrihan@hotmail.fr>
2019-09-17 23:43:02 +0200

    TPTest ex5

--- mise-a-niveau/javascript/TPTest/exercice5.js
+++ mise-a-niveau/javascript/TPTest/exercice5.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-operators */
 // letter to an ascii code
 const c2a = (s) => s.charCodeAt(0);
 
@@ -6,7 +7,7 @@ const a2c = (a) => String.fromCharCode(a);
 
 const caesar = (str, cle) => {
   let res = '';
-  let a = c2a('a');
+  const a = c2a('a');
 
   for (let i = 0, n = str.length; i < n; i += 1) {
     if (str[i] >= 'a' && str[i] <= 'z') {
