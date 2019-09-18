f4eef4471c693ef7c65afa0e8dac4e03c4e01bf1
Loic Trihan <lolotrihan@hotmail.fr>
2019-09-18 14:22:52 +0200

    TP4-2 in progress

--- mise-a-niveau/javascript/TP4/TP4 - exercice 2/js/comportements.js
+++ mise-a-niveau/javascript/TP4/TP4 - exercice 2/js/comportements.js
@@ -42,3 +42,39 @@ const toutReafficherEx4 = document.querySelector('#ex4 .control');
 toutReafficherEx4.addEventListener('click', () => {
   liEx4.forEach((li) => li.classList.remove('invisible'));
 });
+
+// Exercice 5
+
+const ulEx5 = document.querySelector('#lettresEtNombres');
+const liEx5 = document.querySelectorAll('#lettresEtNombres li');
+const toutReafficherEx5 = document.querySelector('#ex5 .control');
+
+const checkClassList = (li) => li.classList.value;
+
+
+const liClickedClass = () => {
+  liEx5.forEach((li) => {
+    // quand je clique sur un li
+    // je veux récupérer sa classe
+    li.addEventListener('click', () => checkClassList(li));
+  });
+};
+
+liEx5.forEach((li) => {
+  // pour cacher tous les autres li ayant la même classe
+  if (li.classList.contains(liClickedClass)) {
+    li.classList.add('invisible');
+  }
+});
+
+
+// Exercice 6
+
+const ulEx6 = document.querySelector('#ex6 ul');
+const liEx6 = document.querySelectorAll('#ex6 ul li');
+
+const moveLiEx6 = () => ulEx6.appendChild(liEx6);
+
+liEx6.forEach((li) => {
+  li.addEventListener('click', moveLiEx6);
+});
