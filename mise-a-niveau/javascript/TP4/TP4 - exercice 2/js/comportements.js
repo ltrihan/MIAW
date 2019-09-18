73a006448eb19d9898244dd3983d3c7b378f4383
Loic Trihan <lolotrihan@hotmail.fr>
2019-09-18 09:06:25 +0200

    TP4-2-3

--- mise-a-niveau/javascript/TP4/TP4 - exercice 2/js/comportements.js
+++ mise-a-niveau/javascript/TP4/TP4 - exercice 2/js/comportements.js
@@ -9,3 +9,22 @@ deleteBtnEx1.addEventListener('click', deleteFirstLi);
 
 // Exercice 2
 
+const boutonDeplacerEx2 = document.querySelector('#boutonDeplacerEx2');
+const liEx2 = document.querySelector('#ex2 ul li');
+const ulEx2 = document.querySelector('#ex2 ul');
+const moveFirstLiEx2 = () => ulEx2.appendChild(liEx2);
+
+boutonDeplacerEx2.addEventListener('click', moveFirstLiEx2);
+
+// Exercice 3                                                             // TO FINISH
+
+const liEx3 = document.querySelector('#ingredients li');
+const ulEx3 = document.querySelector('#ingredients');
+
+const toutReafficherEx3 = document.querySelector('#ex3 .control');
+
+ulEx3.addEventListener('click', liEx3.classList.add('invisible'));
+
+toutReafficherEx3.addEventListener('click', liEx3.classList.remove('invisible'));
+
+// Exercice 4
